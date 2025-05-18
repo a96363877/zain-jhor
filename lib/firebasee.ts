a17809871c99b6d2aedf12eb3f5ac4326c092423
a96363877 <a96363877@gmail.com>
2025-05-19 00:13:16 +0300

    as

--- lib/firebasee.ts
+++ lib/firebasee.ts
@@ -10,7 +10,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
- 
+  apiKey: "AIzaSyC-fR9vsFJ0gyOcysg5SG5DMtATNzTyW1c",
+  authDomain: "exam-app-2ec1d.firebaseapp.com",
+  databaseURL: "https://exam-app-2ec1d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "exam-app-2ec1d",
+  storageBucket: "exam-app-2ec1d.firebasestorage.app",
+  messagingSenderId: "116559499407",
+  appId: "1:116559499407:web:8c5ecf129f5c437e3ea0fa",
+  measurementId: "G-MY09H839C3"
 };
 
 const app = initializeApp(firebaseConfig);
